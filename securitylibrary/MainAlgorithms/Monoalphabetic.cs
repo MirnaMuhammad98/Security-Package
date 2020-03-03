afee448a88e8a372304991741051449935093c9e
Menma <mennaxmohiex@gmail.com>
2020-03-03 20:37:08 +0200

    Update Monoalphabetic.cs

--- securitylibrary/MainAlgorithms/Monoalphabetic.cs
+++ securitylibrary/MainAlgorithms/Monoalphabetic.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,17 +10,69 @@ namespace SecurityLibrary
     {
         public string Analyse(string plainText, string cipherText)
         {
-            throw new NotImplementedException();
+            string key = "";
+
+            plainText = plainText.ToLower();
+            cipherText = cipherText.ToLower();
+
+            Dictionary<char, char> charactersMap = new Dictionary<char, char>();
+            for(int i = 0; i<cipherText.Length; i++)
+            {
+                charactersMap[plainText[i]] = cipherText[i];
+            }
+
+            Queue<char> unMappedChars = new Queue<char>();    
+            for(char i = 'a'; i<='z'; i++)
+            {
+                if (!charactersMap.ContainsValue(i))
+                    unMappedChars.Enqueue(i);
+            }
+
+            for(char i = 'a'; i <= 'z'; i++)
+            {
+                if (!charactersMap.ContainsKey(i))
+                    key += unMappedChars.Dequeue();
+                else
+                    key += charactersMap[i];
+            }
+            return key;
         }
 
         public string Decrypt(string cipherText, string key)
         {
-            throw new NotImplementedException();
+            string plainText = "";
+
+            Dictionary<char, char> keyMap = GetKeyMap(key);
+            cipherText = cipherText.ToLower();
+
+            foreach (char c in cipherText)
+            {
+                plainText += keyMap[c].ToString();
+            }
+            return plainText;
+        }
+
+        private Dictionary<char, char> GetKeyMap(string key)
+        {
+            Dictionary<char, char> keyMap = new Dictionary<char, char>();
+            int keyLength = key.Length;
+            for (int i=0; i<keyLength; i++)
+            {
+                char c = key[i];
+                keyMap[c] = (char)(i +'a');
+            }
+            return keyMap;
         }
 
         public string Encrypt(string plainText, string key)
         {
-            throw new NotImplementedException();
+            string cipherText = "";
+            foreach(char c in plainText)
+            {
+                int charIdx = c - 'a';
+                cipherText += key[charIdx].ToString();
+            }
+            return cipherText;
         }
 
         /// <summary>
@@ -56,7 +108,62 @@ namespace SecurityLibrary
         /// <returns>Plain text</returns>
         public string AnalyseUsingCharFrequency(string cipher)
         {
-            throw new NotImplementedException();
+            Queue<char> charFrequencies = GetFrequencies();
+
+            cipher = cipher.ToLower();
+            Dictionary<char, int> cipherFrequencies = new Dictionary<char, int>();
+            foreach (char c in cipher)
+            {
+                if (cipherFrequencies.ContainsKey(c))
+                    cipherFrequencies[c]++;
+                else
+                    cipherFrequencies.Add(c, 1);
+            }
+
+            cipherFrequencies = cipherFrequencies.OrderByDescending(o => o.Value).ToDictionary(k => k.Key, v => v.Value);
+
+            string key = "";
+            Dictionary<char, char> mappedChars = new Dictionary<char, char>();
+            foreach(char c in cipherFrequencies.Keys)
+                mappedChars[c] = charFrequencies.Dequeue();
+            
+
+            foreach(char c in cipher)
+                key += mappedChars[c];
+
+            return key.ToLower();
+        }
+
+        private Queue<char> GetFrequencies()
+        {
+            Queue<char> frequencies = new Queue<char>();
+            frequencies.Enqueue('E');
+            frequencies.Enqueue('T');
+            frequencies.Enqueue('A');
+            frequencies.Enqueue('O');
+            frequencies.Enqueue('I');
+            frequencies.Enqueue('N');
+            frequencies.Enqueue('S');
+            frequencies.Enqueue('R');
+            frequencies.Enqueue('H');
+            frequencies.Enqueue('L');
+            frequencies.Enqueue('D');
+            frequencies.Enqueue('C');
+            frequencies.Enqueue('U');
+            frequencies.Enqueue('M');
+            frequencies.Enqueue('F');
+            frequencies.Enqueue('P');
+            frequencies.Enqueue('G');
+            frequencies.Enqueue('W');
+            frequencies.Enqueue('Y');
+            frequencies.Enqueue('B');
+            frequencies.Enqueue('V');
+            frequencies.Enqueue('K');
+            frequencies.Enqueue('X');
+            frequencies.Enqueue('J');
+            frequencies.Enqueue('Q');
+            frequencies.Enqueue('Z');
+            return frequencies;
         }
     }
 }
