72b1a0ead46527bdc801b97de2694908d4ca0c85
Menna Mohie <mennaxmohiex@gmail.com>
2020-04-17 00:03:34 +0200

    Initial DES implementation

--- securitylibrary/DES/DES.cs
+++ securitylibrary/DES/DES.cs
@@ -4,21 +4,201 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace SecurityLibrary.DES
-{
+namespace SecurityLibrary.DES {
     /// <summary>
     /// If the string starts with 0x.... then it's Hexadecimal not string
     /// </summary>
-    public class DES : CryptographicTechnique
-    {
-        public override string Decrypt(string cipherText, string key)
-        {
+    public class DES : CryptographicTechnique {
+        public override string Decrypt(string cipherText, string key) {
             throw new NotImplementedException();
         }
 
-        public override string Encrypt(string plainText, string key)
-        {
+        public override string Encrypt(string plainText, string key) {
+            ulong[] binaryBlocks = StringToBinary(plainText);
+            foreach(ulong binaryBlock in binaryBlocks) {
+
+            }
+            
             throw new NotImplementedException();
         }
+        ulong[] StringToBinary(string text) { // ulong = 64 bits
+            if (text[0] == '0' && text[1] == 'x')
+                text = Convert.ToString(Convert.ToInt32(text));
+            byte[] textBytes = System.Text.ASCIIEncoding.ASCII.GetBytes(text);
+            ulong[] textBlocks = new ulong[Convert.ToInt32(Math.Ceiling(textBytes.Length / 8.0))];
+            int currentBlock = -1;
+            for (int i = 0; i < textBytes.Length; i++) {
+                if (i % 8 == 0)
+                    currentBlock++;
+                textBlocks[currentBlock] = 0;
+                textBlocks[currentBlock] = textBlocks[currentBlock] << 8;
+                textBlocks[currentBlock] |= textBytes[i];
+            }
+            return textBlocks;
+        }
+
+        ulong EncryptBlock(ulong block, ulong key) {
+            Halves splitBlock = SplitBlock(block);
+            for(int i = 0; i < 16; i++) {
+                ulong newLeft = splitBlock.Right;
+                ulong newRight = splitBlock.Left ^ Round(splitBlock.Right, key);
+            }
+            throw new NotImplementedException();
+        }
+
+        Halves SplitBlock(ulong block) {
+            Halves splitBlock = new Halves();
+            splitBlock.Left = block >> 32 << 32;
+            splitBlock.Right = block << 32;
+            return splitBlock;
+        }
+
+        ulong Round(ulong rightHalf, ulong key) {
+            ulong expandedRightHalf = Permute(rightHalf, ref expansionTable);
+            ulong xorRes = expandedRightHalf ^ key;
+
+            throw new NotImplementedException();
+        }
+
+        ulong Permute(ulong binaryBlock, ref int[] permTable) {
+            ulong permutedBlock = 0;
+            int blockSize = 64;
+            for (int i = 0; i < permTable.Length; i++) {
+                int pos = permTable[i];
+                ulong bit = (binaryBlock >> (blockSize - pos)) & 1;
+                permutedBlock |= bit << (blockSize - 1 - i);
+            }
+            return permutedBlock;
+        }
+
+        struct Halves {
+            public ulong Left;
+            public ulong Right;
+        }
+        #region DES Tables
+
+        int[] initialPermTable = new int[64] {
+            58,50,42,34,26,18,10,2,
+            60,52,44,36,28,20,12,4,
+            62,54,46,38,30,22,14,6,
+            64,56,48,40,32,24,16,8,
+            57,49,41,33,25,17,9,1,
+            59,51,43,35,27,19,11,3,
+            61,53,45,37,29,21,13,5,
+            63,55,47,39,31,23,15,7
+        };
+
+        int[] inverseInitialPermTable = new int[64] {
+            40, 8, 48, 16, 56, 24, 64, 32,
+            39, 7, 47, 15, 55, 23, 63, 31,
+            38, 6, 46, 14, 54, 22, 62, 30,
+            37, 5, 45, 13, 53, 21, 61, 29,
+            36, 4, 44, 12, 52, 20, 60, 28,
+            35, 3, 43, 11, 51, 19, 59, 27,
+            34, 2, 42, 10, 50, 18, 58, 26,
+            33, 1, 41,  9, 49, 17, 57, 25,
+        };
+
+        #region Key Tables
+
+        int[] permChoice1Table = new int[56] {
+            57,49,41,33,25,17,9,
+            1,58,50,42,34,26,18,
+            10,2,59,51,43,35,27,
+            19,11,3,60,52,44,36,
+            63,55,47,39,31,23,15,
+            7,62,54,46,38,30,22,
+            14,6,61,53,45,37,29,
+            21,13,5,28,20,12,4
+        };
+
+        int[] permChoice2Table = new int[48] {
+            14,17,11,24,1,5,
+            3,28,15,6,21,10,
+            23,19,12,4,26,8,
+            16,7,27,20,13,2,
+            41,52,31,37,47,55,
+            30,40,51,45,33,48,
+            44,49,39,56,34,53,
+            46,42,50,36,29,32
+        };
+
+        int[] LeftShifts = new int[16] { 1, 1, 2, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 2, 1 };
+
+        #endregion
+
+        #region Round Tables
+
+        int[] expansionTable = new int[48] {
+            32,1,2,3,4,5,4,5,
+          6,7,8,9,8,9,10,11,
+          12,13,12,13,14,15,16,17,
+          16,17,18,19,20,21,20,21,
+          22,23,24,25,24,25,26,27,
+          28,29,28,29,30,31,32,1
+        };
+
+        int[] permutationTable = new int[32] {
+            16,7,20,21,29,12,28,17,
+            1,15,23,26,5,18,31,10,
+            2,8,24,14,32,27,3,9,
+            19,13,30,6,22,11,4,25
+        };
+
+        int[][,] SBoxes = { // Array of 2D Arrays
+            new int[,] {
+                {14, 4, 13, 1, 2, 15, 11, 8, 3, 10, 6, 12, 5, 9, 0, 7},
+                {0, 15, 7, 4, 14, 2, 13, 1, 10, 6, 12, 11, 9, 5, 3, 8},
+                {4, 1, 14, 8, 13, 6, 2, 11, 15, 12, 9, 7, 3, 10, 5, 0},
+                {15, 12, 8, 2, 4, 9, 1, 7, 5, 11, 3, 14, 10, 0, 6, 13}
+            },
+            new int[,] {
+                { 15, 1, 8, 14, 6, 11, 3, 4, 9, 7, 2, 13, 12, 0, 5, 10},
+                {3, 13, 4, 7, 15, 2, 8, 14, 12, 0, 1, 10, 6, 9, 11, 5},
+                {0, 14, 7, 11, 10, 4, 13, 1, 5, 8, 12, 6, 9, 3, 2, 15},
+                {13, 8, 10, 1, 3, 15, 4, 2, 11, 6, 7, 12, 0, 5, 14, 9}
+            },
+            new int[,] {
+                { 10, 0, 9, 14, 6, 3, 15, 5, 1, 13, 12, 7, 11, 4, 2, 8},
+                {13, 7, 0, 9, 3, 4, 6, 10, 2, 8, 5, 14, 12, 11, 15, 1 },
+                {13, 6, 4, 9, 8, 15, 3, 0, 11, 1, 2, 12, 5, 10, 14, 7 },
+                {1, 10, 13, 0, 6, 9, 8, 7, 4, 15, 14, 3, 11, 5, 2, 12 }
+            },
+            new int[,] {
+                { 7, 13, 14, 3, 0, 6, 9, 10, 1, 2, 8, 5, 11, 12, 4, 15},
+                {13, 8, 11, 5, 6, 15, 0, 3, 4, 7, 2, 12, 1, 10, 14, 9 },
+                {10, 6, 9, 0, 12, 11, 7, 13, 15, 1, 3, 14, 5, 2, 8, 4 },
+                {3, 15, 0, 6, 10, 1, 13, 8, 9, 4, 5, 11, 12, 7, 2, 14 }
+            },
+            new int[,] {
+                {2, 12, 4, 1, 7, 10, 11, 6, 8, 5, 3, 15, 13, 0, 14, 9},
+                {14, 11, 2, 12, 4, 7, 13, 1, 5, 0, 15, 10, 3, 9, 8, 6},
+                {4, 2, 1, 11, 10, 13, 7, 8, 15, 9, 12, 5, 6, 3, 0, 14},
+                {11, 8, 12, 7, 1, 14, 2, 13, 6, 15, 0, 9, 10, 4, 5, 3}
+            },
+            new int[,] {
+                {12, 1, 10, 15, 9, 2, 6, 8, 0, 13, 3, 4, 14, 7, 5, 11},
+                {10, 15, 4, 2, 7, 12, 9, 5, 6, 1, 13, 14, 0, 11, 3, 8},
+                {9, 14, 15, 5, 2, 8, 12, 3, 7, 0, 4, 10, 1, 13, 11, 6},
+                {4, 3, 2, 12, 9, 5, 15, 10, 11, 14, 1, 7, 6, 0, 8, 13}
+            },
+            new int[,] {
+                {4, 11, 2, 14, 15, 0, 8, 13, 3, 12, 9, 7, 5, 10, 6, 1},
+                {13, 0, 11, 7, 4, 9, 1, 10, 14, 3, 5, 12, 2, 15, 8, 6},
+                {1, 4, 11, 13, 12, 3, 7, 14, 10, 15, 6, 8, 0, 5, 9, 2},
+                {6, 11, 13, 8, 1, 4, 10, 7, 9, 5, 0, 15, 14, 2, 3, 12}
+            },
+            new int[,] {
+                {13, 2, 8, 4, 6, 15, 11, 1, 10, 9, 3, 14, 5, 0, 12, 7},
+                {1, 15, 13, 8, 10, 3, 7, 4, 12, 5, 6, 11, 0, 14, 9, 2},
+                {7, 11, 4, 1, 9, 12, 14, 2, 0, 6, 10, 13, 15, 3, 5, 8},
+                {2, 1, 14, 7, 4, 10, 8, 13, 15, 12, 9, 0, 3, 5, 6, 11}
+            }
+        };
+
+        #endregion
+
+        #endregion
+
     }
 }
